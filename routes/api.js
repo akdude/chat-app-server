54361c9e70af35ef70d5622c3b27b1bbf151a0a8
abishek <abishek@terareach.com>
2020-12-13 19:52:46 +0530

    2nd

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Pusher = require('pusher');
 
+// Initializing pusher service for web socket communication
 const pusher = new Pusher({
   app_id : "1052554",
   key : "32a680012e60b375e014",
@@ -9,11 +10,6 @@ const pusher = new Pusher({
   cluster : "ap2"
 });
 
-let messages = [];
-
-router.get('/', (req, res) => {
-  res.send('all good');
-});
 
 router.post('/pusher/auth', (req, res) => {
   const socketId = req.body.socket_id;
